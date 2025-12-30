12641578b76098a71b721cb0195629f9c1148b99
90degs2infty <90degs2infty@posteo.org>
2025-12-30 21:03:20 +0100

    feat: introduce date of publication to frontmatter
    Frontmatters (both index and page) now require a ISO 8601-formatted date of
    publication to be specified under the key `publication`.

--- loveletters_lib/src/content.rs
+++ loveletters_lib/src/content.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use time::{OffsetDateTime, PrimitiveDateTime};
 use typst::foundations::{Dict, IntoValue, Value};
 
 // TODO: dedicated module?
@@ -6,16 +7,26 @@ use typst::foundations::{Dict, IntoValue, Value};
 #[derive(Debug, Deserialize, Serialize)]
 pub struct IndexFrontmatter {
     title: String,
+    #[serde(with = "time::serde::iso8601")]
+    publication: OffsetDateTime,
+    // TODO expiry: OffsetDateTime,
 }
 
 impl IndexFrontmatter {
     pub fn to_typst(&self) -> Value {
-        let Self { title } = self;
+        let Self { title, publication } = self;
 
         let mut d = Dict::new();
         d.insert(
             "title".into(),
             typst::foundations::Value::Str(title.as_str().into()),
+        );
+        d.insert(
+            "publication".into(),
+            typst::foundations::Value::Datetime(typst::foundations::Datetime::Datetime(
+                // TODO is this the intended way to (serde) deserialize a date and get a datetime from it?
+                PrimitiveDateTime::new(publication.date(), publication.time()),
+            )),
         );
         Value::Dict(d)
     }
@@ -30,16 +41,26 @@ impl<'a> IntoValue for &'a IndexFrontmatter {
 #[derive(Debug, Deserialize, Serialize)]
 pub struct LeafFrontmatter {
     title: String,
+    #[serde(with = "time::serde::iso8601")]
+    publication: OffsetDateTime,
+    // TODO expiry: OffsetDateTime,
 }
 
 impl LeafFrontmatter {
     pub fn to_typst(&self) -> Value {
-        let Self { title } = self;
+        let Self { title, publication } = self;
 
         let mut d = Dict::new();
         d.insert(
             "title".into(),
             typst::foundations::Value::Str(title.as_str().into()),
+        );
+        d.insert(
+            "publication".into(),
+            typst::foundations::Value::Datetime(typst::foundations::Datetime::Datetime(
+                // TODO is this the intended way to (serde) deserialize a date and get a datetime from it?
+                PrimitiveDateTime::new(publication.date(), publication.time()),
+            )),
         );
         Value::Dict(d)
     }
