4d8aaa91af32ec0076607acf915db9711a237fd9
90degs2infty <90degs2infty@posteo.org>
2026-03-21 17:51:40 +0100

    Dump current test suite

--- loveletters_testsuite/tests/default.rs
+++ loveletters_testsuite/tests/default.rs
@@ -4,10 +4,13 @@ use loveletters_lib::{
     error::{EntityKind, Error, Result},
     render_dir,
 };
-use loveletters_testsuite::mock::{LeafFrontmatter, LeafPage, Project, Section, TypstFile};
+use loveletters_testsuite::mock::{
+    LeafFrontmatter, LeafPage, Project, ProjectConfig, Section, Slug, TypstFile,
+};
 use proptest::prelude::*;
-use std::{fs, mem, path::PathBuf};
+use std::mem;
 use tempfile::{Builder, TempDir};
+use test_strategy::proptest;
 
 macro_rules! prop_assert_matches {
     ( $e:expr , $pat:pat ) => {{
@@ -22,33 +25,56 @@ macro_rules! prop_assert_matches {
     }};
 }
 
+#[derive(Debug)]
+enum Location {
+    ReplaceLeaf(usize),
+    AppendToSection(usize, Slug),
+}
+
+fn attach_mutation_location(sec: Section) -> impl Strategy<Value = (Section, Location)> {
+    if sec.num_leafs() > 0 {
+        let num_leafs = sec.num_leafs();
+        (Just(sec), (0..num_leafs).prop_map(Location::ReplaceLeaf)).boxed()
+    } else {
+        let num_sections = sec.num_sections();
+        (
+            Just(sec),
+            (0..num_sections, Slug::valid())
+                .prop_map(|(idx, slug)| Location::AppendToSection(idx, slug)),
+        )
+            .boxed()
+    }
+}
+
 fn replace_random_leaf(
     section: impl Strategy<Value = Section>,
     leaf: impl Strategy<Value = LeafPage>,
 ) -> impl Strategy<Value = Section> {
-    (
-        section
-            // Note on filter: it is somewhat unlikely to encounter a "recursively" empty section,
-            // i.e. a section where the section itself and all subsections do not feature any leaf
-            // pages. Thus, we stick to the filter for the moment. Another approach is to just
-            // append a new (invalid) page at some arbitrary location in case one encounters an
-            // empty section.
-            //
-            // TODO: implement the second approach, as this is very likely to be encountered in case
-            // of shrinking on a failing testcase!
-            .prop_filter("(recursively) empty section, no leaf to replace", |sec| {
-                sec.num_leafs() != 0
-            })
-            .prop_flat_map(|sec| {
-                let num_leafs = sec.num_leafs();
-                (Just(sec), 0..num_leafs)
-            }),
-        leaf,
+    (section.prop_flat_map(attach_mutation_location), leaf).prop_flat_map(
+        |((mut sec, loc), leaf)| match loc {
+            Location::ReplaceLeaf(idx) => {
+                let _ = mem::replace(
+                    sec.leaf_at_mut(idx)
+                        .expect("index should point to valid leaf page"),
+                    leaf,
+                );
+                Just(sec)
+            }
+            Location::AppendToSection(idx, slug) => {
+                let previous = sec
+                    .section_at_mut(idx)
+                    .expect("index should point to valid (sub-)section")
+                    .insert_leaf(slug, leaf);
+
+                // As there is no expect_none (yet), we panic by hand...
+                if let Some(_) = previous {
+                    panic!("key should not point to pre-existing leaf page");
+                }
+
+                Just(sec)
+            }
+        },
     )
-        .prop_flat_map(|((mut sec, idx), leaf)| {
-            let _ = mem::replace(sec.leaf_at_mut(idx).expect("msg"), leaf);
-            Just(sec)
-        })
 }
 
 fn setup_testcase(project: &Project) -> (TempDir, TempDir) {
@@ -64,27 +90,87 @@ fn render_project(project: &Project) -> (TempDir, TempDir, Result<()>) {
     let res = render_dir(&input, &output);
     (input, output, res)
 }
+#[proptest]
+fn project_requires_configuration(#[strategy(Project::missing_config())] project: Project) {
+    let (_input, _output, res) = render_project(&project);
 
-// TODO: use the `test_cases` crate instead of the `proptest!` macro?
-proptest! {
-    #[test]
-    fn project_requires_configuration(project in Project::missing_config()) {
-        let (_input, _output, res) = render_project(&project);
-
-        prop_assert_matches!(
-            res,
-            Err(Error::NotFound { missing: EntityKind::ProjectConfig, path: _ })
-        )
-    }
-
-    #[test]
-    fn project_requires_content(project in Project::missing_content()) {
-        let (_input, _output, res) = render_project(&project);
-
-        prop_assert_matches!(
-            res,
-            Err(Error::NotFound { missing: EntityKind::ContentDirectory, path: _ })
-        )
-    }
-
+    prop_assert_matches!(
+        res,
+        Err(Error::NotFound {
+            missing: EntityKind::ProjectConfig,
+            path: _
+        })
+    )
 }
+
+#[proptest]
+fn project_requires_content(#[strategy(Project::missing_content())] project: Project) {
+    let (_input, _output, res) = render_project(&project);
+
+    prop_assert_matches!(
+        res,
+        Err(Error::NotFound {
+            missing: EntityKind::ContentDirectory,
+            path: _
+        })
+    )
+}
+
+// TODO: does it make more sense to have dedicated test cases for distinct cases of broken project configs?
+// Given the number of iterations during test case execution: probably not
+#[proptest]
+fn project_requires_valid_frontmatter(
+    #[strategy(
+        Project::general(
+            Section::valid().prop_map(Option::Some),
+            ProjectConfig::invalid().prop_map(Option::Some)
+        )
+    )]
+    project: Project,
+) {
+    let (_input, _output, res) = render_project(&project);
+
+    prop_assert_matches!(
+        res,
+        Err(Error::MalformedProjectConfig {
+            location: _,
+            raw: _
+        })
+    )
+}
+
+#[proptest]
+fn leaf_page_requires_valid_frontmatter(
+    #[strategy(
+        Project::general(
+            replace_random_leaf(
+                Section::toplevel_and_posts(),
+                LeafPage::general(
+                    LeafFrontmatter::invalid().prop_map(Option::Some),
+                    TypstFile::valid().prop_map(Option::Some)
+                )
+            ).prop_map(Option::Some),
+            ProjectConfig::valid().prop_map(Option::Some)
+        )
+    )]
+    project: Project,
+) {
+    let (_input, _output, res) = render_project(&project);
+
+    prop_assert_matches!(
+        res,
+        Err(Error::MalformedFrontmatter {
+            location: _,
+            raw: _
+        })
+    )
+}
+
+// Further test cases
+//
+// - page inside section directory
+// - broken project config
+//   - subcases individually or the general invalid strategy?
+// - section without index page (should not error, but not create any output for said section)
+// - section with broken index front matter
+// - leaf page without frontmatter file (should not error, but not create any output for said page)
