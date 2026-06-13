2e945b941c0cfb4ae455571d3130b44910db71ba
90degs2infty <90degs2infty@posteo.org>
2026-06-13 09:53:06 +0200

    Implement some integration tests for toplevel project config

---
+++ loveletters_lib/tests/project_config.rs
@@ -0,0 +1,208 @@
+//! Integration tests targeting a project's toplevel configuration file.
+
+use anyhow::{Context, Result};
+use lattice::IntoDefect;
+use loveletters_lib::error::{EntityKind, Error};
+use loveletters_lib::render_dir;
+use loveletters_mock::project::{Project, ProjectStrategyBuilder};
+use loveletters_test_helpers::unexpected::Unexpected;
+use proptest::prelude::*;
+use proptest_ext::conversion::IntoProptest;
+use std::result::Result as StdResult;
+use tempfile::TempDir;
+use test_strategy::proptest;
+
+async fn try_render_mock(mock: &Project) -> Result<StdResult<(), Error>> {
+    let input_dir = TempDir::with_prefix("loveletters-")
+        .with_context(|| "while creating a temporary input directory")?;
+
+    let output_dir = TempDir::with_prefix("loveletters-")
+        .with_context(|| "while creating a temporary output directory")?;
+
+    mock.try_write_to_dir(input_dir.as_ref())
+        .await
+        .with_context(|| {
+            format!(
+                "while writing project to temporary directory {}",
+                input_dir.as_ref().display()
+            )
+        })?;
+
+    let res = render_dir(input_dir.path(), output_dir.path());
+
+    Ok(res)
+}
+
+#[proptest(async = "tokio")]
+async fn project_requires_config(
+    #[strategy(
+        {
+            ProjectStrategyBuilder::empty().without_config().build()
+        }
+    )]
+    mock: Project,
+) {
+    let res = try_render_mock(&mock).await.into_proptest()?;
+
+    match res {
+        Err(Error::NotFound {
+            missing: EntityKind::ProjectConfig,
+            path: _,
+        }) => Ok(()),
+        r => Err(Unexpected::from(r)),
+    }?;
+}
+
+#[proptest(async = "tokio")]
+async fn project_config_requires_loveletters_filestem(
+    #[strategy(
+        {
+            let mut project = ProjectStrategyBuilder::empty();
+            project.config_mut().expect("empty project should have a toplevel configuration").with_filestem("[a-z]{4}".boxed());
+            project.build()
+        }
+    )]
+    mock: Project,
+) {
+    let res = try_render_mock(&mock).await.into_proptest()?;
+
+    match res {
+        Err(Error::NotFound {
+            missing: EntityKind::ProjectConfig,
+            path: _,
+        }) => Ok(()),
+        r => Err(Unexpected::from(r)),
+    }?;
+}
+
+#[proptest(async = "tokio")]
+async fn project_config_requires_toml_fileext(
+    #[strategy(
+        {
+            let mut project = ProjectStrategyBuilder::empty();
+            project.config_mut().expect("empty project should have a toplevel configuration").with_filestem("[a-z]{0,3}".boxed());
+            project.build()
+        }
+    )]
+    mock: Project,
+) {
+    let res = try_render_mock(&mock).await.into_proptest()?;
+
+    match res {
+        Err(Error::NotFound {
+            missing: EntityKind::ProjectConfig,
+            path: _,
+        }) => Ok(()),
+        r => Err(Unexpected::from(r)),
+    }?;
+}
+
+#[proptest(async = "tokio")]
+async fn project_config_requires_title(
+    #[strategy(
+        {
+            let mut project = ProjectStrategyBuilder::empty();
+            project.config_mut().expect("empty project should have a toplevel configuration").without_title();
+            project.build()
+        }
+    )]
+    mock: Project,
+) {
+    let res = try_render_mock(&mock).await.into_proptest()?;
+
+    match res {
+        Err(Error::MalformedProjectConfig {
+            location: _,
+            raw: _,
+        }) => Ok(()),
+        r => Err(Unexpected::from(r)),
+    }?;
+}
+
+#[proptest(async = "tokio")]
+async fn project_config_requires_author(
+    #[strategy(
+        {
+            let mut project = ProjectStrategyBuilder::empty();
+            project.config_mut().expect("empty project should have a toplevel configuration").without_author();
+            project.build()
+        }
+    )]
+    mock: Project,
+) {
+    let res = try_render_mock(&mock).await.into_proptest()?;
+
+    match res {
+        Err(Error::MalformedProjectConfig {
+            location: _,
+            raw: _,
+        }) => Ok(()),
+        r => Err(Unexpected::from(r)),
+    }?;
+}
+
+#[proptest(async = "tokio")]
+async fn project_config_requires_root(
+    #[strategy(
+        {
+            let mut project = ProjectStrategyBuilder::empty();
+            project.config_mut().expect("empty project should have a toplevel configuration").without_root();
+            project.build()
+        }
+    )]
+    mock: Project,
+) {
+    let res = try_render_mock(&mock).await.into_proptest()?;
+
+    match res {
+        Err(Error::MalformedProjectConfig {
+            location: _,
+            raw: _,
+        }) => Ok(()),
+        r => Err(Unexpected::from(r)),
+    }?;
+}
+
+#[proptest(async = "tokio")]
+async fn project_config_requires_valid_root(
+    #[strategy(
+        {
+            let mut project = ProjectStrategyBuilder::empty();
+            project.config_mut().expect("empty project should have a toplevel configuration").with_root("[a-z]*".into_defect().boxed());
+            project.build()
+        }
+    )]
+    mock: Project,
+) {
+    let res = try_render_mock(&mock).await.into_proptest()?;
+
+    match res {
+        Err(Error::MalformedProjectConfig {
+            location: _,
+            raw: _,
+        }) => Ok(()),
+        r => Err(Unexpected::from(r)),
+    }?;
+}
+
+#[proptest(async = "tokio")]
+async fn project_config_denies_excess_project_config_key(
+    #[strategy(
+        {
+            let mut project = ProjectStrategyBuilder::empty();
+            project.config_mut().expect("empty project should have a toplevel configuration").with_excess("[a-z]{0,4}".prop_map(Some).boxed());
+            project.build()
+        }
+    )]
+    mock: Project,
+) {
+    let res = try_render_mock(&mock).await.into_proptest()?;
+
+    match res {
+        Err(Error::MalformedProjectConfig {
+            location: _,
+            raw: _,
+        }) => Ok(()),
+        r => Err(Unexpected::from(r)),
+    }?;
+}
