c8898e3700ec84a1c3bdb81b35da7384c1e1279a
90degs2infty <90degs2infty@posteo.org>
2026-06-13 13:54:09 +0200

    Extend documentation

--- loveletters_test_helpers/src/lib.rs
+++ loveletters_test_helpers/src/lib.rs
@@ -1,3 +1,5 @@
+//! Helpers for implementing tests targeting `loveletters`.
+
 pub mod mismatch;
 
 use anyhow::{Context, Result};
@@ -6,6 +8,19 @@ use loveletters_mock::project::Project;
 use std::result::Result as StdResult;
 use tempfile::TempDir;
 
+/// Try to render the specified `mock` project.
+///
+/// Writes the specified `mock` to a temporary directory and renders the resulting content to a
+/// temporary output directory.
+/// Returns the result of calling [`render_dir`] (wrapped as `Ok(res)`) in case content preparation
+/// is successful.
+///
+/// # Errors
+///
+/// Returns an error if for some reason writing `mock` fails.
+///
+/// Note that potential errors returned by [`render_dir`] are _not_ returned as error!
+/// I.e. the result returned by [`render_dir`] is passed on unchanged wrapped in `Ok(res)`.
 pub async fn try_render_mock(mock: &Project) -> Result<StdResult<(), Error>> {
     let input_dir = TempDir::with_prefix("loveletters-")
         .with_context(|| "while creating a temporary input directory")?;
